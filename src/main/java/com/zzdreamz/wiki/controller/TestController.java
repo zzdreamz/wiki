a9f210880898af8b0e120e3269dffd13d4303022
zzdreamz <13535327736@163.com>
2021-08-11 17:00:12 +0800

    开发Hello World接口

---
+++ src/main/java/com/zzdreamz/wiki/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.zzdreamz.wiki.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World!!!";
+    }
+}
