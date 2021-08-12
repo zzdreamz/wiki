858f77a08288c1d4def9c41e03d32a9992c9fbbf
zzdreamz <13535327736@163.com>
2021-08-12 14:52:49 +0800

    添加通用返回类，修改电子书列表查询接口

--- src/main/java/com/zzdreamz/wiki/controller/EbookController.java
+++ src/main/java/com/zzdreamz/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.zzdreamz.wiki.controller;
 
 import com.zzdreamz.wiki.domain.Ebook;
+import com.zzdreamz.wiki.resp.CommonResp;
 import com.zzdreamz.wiki.service.EbookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,13 +15,11 @@ public class EbookController {
     @Autowired
     private EbookService ebookService;
 
-    @RequestMapping("/ebook/hello")
-    public String hello() {
-        return "Hello World, zzdreamz!!!";
-    }
-
     @RequestMapping("/ebook/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
