3be2c52100ce998ea600b93de19f49162f7f418f
zzdreamz <13535327736@163.com>
2021-08-12 14:26:42 +0800

    集成mybatis-generator官方代码生成器，完成Demo表的测试

--- doc/wiki.sql
+++ doc/wiki.sql
@@ -6,3 +6,12 @@ create table `test` (
     `password` varchar(50) comment '密码',
     primary key(`id`)
 )engine=innodb default charset=utf8mb4 comment='测试';
+
+drop table if exists `demo`;
+create table `demo`(
+    `id` bigint not null comment 'id',
+    `name` varchar (50) comment '名称',
+    primary key (`id`)
+)engine=innodb default charset=utf8mb4 comment'测试';
+
+insert into `demo`(id,name) values(1,'测试');
--- src/main/java/com/zzdreamz/wiki/controller/DemoController.java
+++ src/main/java/com/zzdreamz/wiki/controller/DemoController.java
@@ -1,7 +1,7 @@
 package com.zzdreamz.wiki.controller;
 
-import com.zzdreamz.wiki.domain.Test;
-import com.zzdreamz.wiki.service.TestService;
+import com.zzdreamz.wiki.domain.Demo;
+import com.zzdreamz.wiki.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,18 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-public class TestController {
+public class DemoController {
 
     @Autowired
-    private TestService testService;
+    private DemoService demoService;
 
-    @RequestMapping("/test/hello")
+    @RequestMapping("/demo/hello")
     public String hello() {
         return "Hello World, zzdreamz!!!";
     }
 
-    @RequestMapping("/test/list")
-    public List<Test> list() {
-        return testService.list();
+    @RequestMapping("/demo/list")
+    public List<Demo> list() {
+        return demoService.list();
     }
 }
