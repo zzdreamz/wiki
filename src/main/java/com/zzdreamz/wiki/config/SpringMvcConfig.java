da22b1fdfcaa2a2d4d4cf95432f364b6f3a075b4
zzdreamz <13535327736@163.com>
2021-08-13 11:16:12 +0800

    配置拦截器，打印请求耗时

---
+++ src/main/java/com/zzdreamz/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,20 @@
+package com.zzdreamz.wiki.config;
+
+import com.zzdreamz.wiki.interceptor.LogInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LogInterceptor logInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**");
+    }
+}
