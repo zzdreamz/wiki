175e6bc2ca5ecd3641354b1fe0bbb7a3def75470
zzdreamz <13535327736@163.com>
2021-08-13 21:56:43 +0800

    添加查询请求参数校验

--- src/main/java/com/zzdreamz/wiki/controller/EbookController.java
+++ src/main/java/com/zzdreamz/wiki/controller/EbookController.java
@@ -9,6 +9,8 @@ import com.zzdreamz.wiki.service.EbookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/ebook")
 public class EbookController {
@@ -17,7 +19,7 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public CommonResp list(EbookQueryReq req) {
+    public CommonResp list(@Valid EbookQueryReq req) {
         CommonResp<PageResp<EbookQueryResp>> resp = new CommonResp<>();
         PageResp<EbookQueryResp> pageResp = ebookService.list(req);
         resp.setContent(pageResp);
--- src/main/java/com/zzdreamz/wiki/req/PageReq.java
+++ src/main/java/com/zzdreamz/wiki/req/PageReq.java
@@ -1,9 +1,15 @@
 package com.zzdreamz.wiki.req;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+
 public class PageReq {
 
+    @NotNull(message = "【页码】不能为空")
     private int pageNum;
 
+    @NotNull(message = "【每页条数】不能为空")
+    @Max(value = 1000, message = "【每页条数】不能超过1000")
     private int pageSize;
 
     public PageReq() {
