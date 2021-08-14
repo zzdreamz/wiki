65c92d621664391eaf9dc823e9f9c55fee9056d6
zzdreamz <13535327736@163.com>
2021-08-14 13:58:43 +0800

    添加分类表并生成持久层代码，分类采用id+parent来设计无限级树

--- doc/wiki.sql
+++ doc/wiki.sql
@@ -1,19 +1,4 @@
-
-drop table if exists `test`;
-create table `test` (
-    `id` bigint not null comment 'id',
-    `name` varchar(50) comment '名称',
-    `password` varchar(50) comment '密码',
-    primary key(`id`)
-)engine=innodb default charset=utf8mb4 comment='测试';
-
-drop table if exists `demo`;
-create table `demo`(
-    `id` bigint not null comment 'id',
-    `name` varchar (50) comment '名称',
-    primary key (`id`)
-)engine=innodb default charset=utf8mb4 comment'测试';
-
+# 电子书表
 drop table if exists `ebook`;
 create table `ebook` (
     `id` bigint not null comment 'id' ,
@@ -28,9 +13,38 @@ create table `ebook` (
     primary key ( id )
 ) engine=innodb default charset=utf8mb4 comment='电子书';
 
+# 电子书表初始数据
 insert into `ebook`(id,name,description) values (1,'Spring Boot 入门教程' ,'零基础入门Java开发,企业级应用开发最佳首选框架');
 insert into `ebook`(id,name,description) values (2,'Vue 入门教程','零基础入门vue 开发,企业级应用开发最住首选框架');
 insert into `ebook`(id,name,description) values (3,'Python 入门教程','零基础入门Python 开发,企业级应用开发最佳首选框架');
 insert into `ebook`(id,name,description) values (4,'mysql 入门教程','零基础入门mysql,企业级应用开发最佳首选框架');
 insert into `ebook`(id,name,description) values (5,'Oracle 入门教程','零基础入门Oracle 开发,企业级应用开发最佳首选框架');
+
+# 分类表
+drop table if exists `category`;
+create table `category`(
+    `id` bigint not null comment 'id' ,
+    `parent` bigint not null default 0 comment '父id',
+    `name` varchar(50) not null comment '名称',
+    `sort` int comment '顺序',
+    primary key ( `id` )
+)engine=innodb default charset=utf8mb4 comment='分类';
+
+# 分类表初始数据
+insert into `category` (id,parent,name,sort) values (100,000,'前端开发',100);
+insert into `category` (id,parent,name,sort) values (101,100,'Vue', 101);
+insert into `category` (id,parent,name,sort) values (102,100,'HTML & CSS',102);
+insert into `category` (id,parent,name,sort) values (200,800,'Java', 200);
+insert into `category` (id,parent,name,sort) values (201,200,'基础应用',201);
+insert into `category` (id,parent,name,sort) values (202,200,'框架应用',202);
+insert into `category` (id,parent,name,sort) values (300,000,'Python', 300);
+insert into `category` (id,parent,name,sort) values (301,300,'基础应用',301);
+insert into `category` (id,parent,name,sort) values (302,300,'进阶方向应用',302);
+insert into `category` (id,parent,name,sort) values (400,000,'数据库',400);
+insert into `category` (id,parent,name,sort) values (401,400,'MysQL',401);
+insert into `category` (id,parent,name,sort) values (500, 000,'其它',500);
+insert into `category` (id,parent,name,sort) values (501,500,'服务器',501);
+insert into `category` (id,parent,name,sort) values (502,500,'开发工具',502);
+insert into `category` (id,parent,name,sort) values (503,500,'热门服务端语言',503);
+
 
--- src/main/java/com/zzdreamz/wiki/mapper/EbookMapper.java
+++ src/main/java/com/zzdreamz/wiki/mapper/EbookMapper.java
@@ -3,8 +3,6 @@ package com.zzdreamz.wiki.mapper;
 import com.zzdreamz.wiki.domain.Ebook;
 import com.zzdreamz.wiki.domain.EbookExample;
 import java.util.List;
-
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 public interface EbookMapper {
