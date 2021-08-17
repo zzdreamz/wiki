58b75c4203a9012fcfd41dbaf7cfa235307b8f0b
zzdreamz <13535327736@163.com>
2021-08-17 21:01:17 +0800

    使用Vuex来保存全局变量

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -56,6 +56,7 @@
 import {defineComponent, ref} from 'vue';
 import axios from "axios";
 import {message} from "ant-design-vue";
+import store from "@/store";
 
 declare let hexMd5: any;
 declare let KEY: any;
@@ -74,7 +75,7 @@ export default defineComponent({
     loginUser.value = {};
     // 登录后的用户信息
     const user = ref();
-    user.value = {};
+    user.value = store.state.user;
 
     const loginModalShow = () => {
       loginModalVisible.value = true;
@@ -89,6 +90,7 @@ export default defineComponent({
         if (data.success) {
           loginModalVisible.value = false;
           user.value = data.content;
+          store.commit('setUser', user.value);
         } else {
           message.error(data.message);
         }
