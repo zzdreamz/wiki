87c6aca4c99b510c32951522c11dbb726ac4c7e3
zzdreamz <13535327736@163.com>
2021-08-17 14:22:03 +0800

    使用MD5来实现加密传输和加密存储

--- src/main/java/com/zzdreamz/wiki/controller/UserController.java
+++ src/main/java/com/zzdreamz/wiki/controller/UserController.java
@@ -7,9 +7,11 @@ import com.zzdreamz.wiki.resp.UserQueryResp;
 import com.zzdreamz.wiki.resp.PageResp;
 import com.zzdreamz.wiki.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/user")
@@ -28,6 +30,7 @@ public class UserController {
 
     @PostMapping("/save")
     public CommonResp save(@Valid @RequestBody UserSaveReq req) {
+        req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp resp = new CommonResp<>();
         userService.save(req);
         return resp;
